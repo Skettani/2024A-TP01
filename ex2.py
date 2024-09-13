6ccbe9cd69e00fd3cd433c8fb583e6b15a051f9f
Vincent Brunette <vincent2004@gmail.com>
2024-09-13 13:06:04 -0400

    exercice 2

--- ex2.py
+++ ex2.py
@@ -1,4 +1,12 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
 
-water_quantity = ...
+water_quantity = float(input("Quelle est la quantité d'eau à assainir :"))
+nombre_de_filtres = ((water_quantity)/5)
+nombre_lampesUV= (water_quantity/5 *3)
+nombre_kgChlore= water_quantity/10
+print("Voici les éléments requis pour assainir ", water_quantity, "L d'eau")
+print("")
+print (" - Lampes UV:" , nombre_lampesUV, )
+print (" - Chlore:",nombre_kgChlore, "Kg" )
+print (" - Filtres : ",nombre_de_filtres)
--- ex4.py
+++ ex4.py
@@ -3,4 +3,38 @@
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
 
-battery_level = ...
+battery_level  = float (input("Pourcentage de batterie?"))
+Distance= float (battery_level)
+#round (float, Distance, 0.1)
+#Distance ;05= float(battery_level *6)
+#5:10= float(battery_level*2.5)
+#10:25= float(battery_level)
+#25:50= float(battery_level*0.5)   
+#50:100= float(battery_level*2)
+
+
+if int (0 < battery_level<= 5):
+    Distance *=6 
+    round (Distance, 1)
+    print ("Distance remaining is ", Distance,"km")
+   
+
+if int  (5 <battery_level <=10):
+    Distance*=2.5
+    round (Distance, 1)
+    print ("Distance remaining is ", Distance ,"km")
+
+if int (10 <battery_level <=25):
+    Distance *=1
+    round (Distance, 2)
+    print ("Distance remaining is ", Distance,"km")
+
+if int  (25 <battery_level <=50):
+    Distance *=0.5
+    round (Distance, 1)
+    print ("Distance remaining is ", Distance *0.5,"km")
+
+if int  (50 <battery_level <=100):
+    Distance *=2
+    round (Distance, 1)
+    print ("Distance remaining is ", Distance *2 ,"km")
