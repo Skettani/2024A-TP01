2f62def3cfd7e68317d9fe10a0c334c04741dfb3
Vincent Brunette <vincent2004@gmail.com>
2024-09-15 21:08:20 -0400

    ex.2 et ex.4

--- ex2.py
+++ ex2.py
@@ -1,12 +1,13 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
-
-water_quantity = float(input("Quelle est la quantité d'eau à assainir :"))
-nombre_de_filtres = ((water_quantity)/5)
-nombre_lampesUV= (water_quantity/5 *3)
-nombre_kgChlore= water_quantity/10
-print("Voici les éléments requis pour assainir ", water_quantity, "L d'eau")
-print("")
-print (" - Lampes UV:" , nombre_lampesUV, )
-print (" - Chlore:",nombre_kgChlore, "Kg" )
-print (" - Filtres : ",nombre_de_filtres)
+import math
+water_quantity = float (input("Quelle quantité d'eau faut-il assainir ? " ))
+nombre_de_filtres =  math.ceil((water_quantity)/5)
+nombre_lampesUV= math.ceil((water_quantity/5 *3))
+nombre_kgChlore= round (water_quantity/10, 2)
+if (water_quantity%1 ==0):
+    water_quantity= round(water_quantity)
+print(f"Voici les éléments requis pour assainir {water_quantity}L d'eau:\n")
+print (f"        \t- Filtre(s) : {nombre_de_filtres}\n")
+print (f"        \t- Lampe(s) UV : {nombre_lampesUV}\n")
+print (f"        \t- Chlore : {nombre_kgChlore}kg")
--- ex4.py
+++ ex4.py
@@ -3,4 +3,42 @@
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
 
-battery_level = ...
+battery_level  = float (input("Pourcentage de batterie ? "))
+Distance = float (0)
+
+if int  (50 <battery_level <=100):
+    Distance += 2 * (battery_level - 50)
+    battery_level = 50
+   
+    
+if int  (25 <battery_level <=50):
+    Distance += 0.5* (battery_level-25) 
+    battery_level = 25
+    
+if int (10 <battery_level <=25):
+    Distance += 1*(battery_level - 10)
+    battery_level = 10
+    
+
+if int  (5 <battery_level <=10):
+    Distance += 2.5* (battery_level - 5)
+    battery_level = 5
+   
+
+if int (0 < battery_level<= 5):
+    Distance +=6*(battery_level) 
+    print (round (Distance, 1),"km")
+
+if int (0 == battery_level):
+    print ("La batterie est vide")
+
+
+
+
+   
+
+
+
+
+
+
